aaa97362c804ea961084fd7e8bf2357ce36cda33
Đặng Quốc Cường <150898842+DPlayerGod@users.noreply.github.com>
2024-12-09 23:30:30 +0700

    Delete FinalReport/CD20_3 directory

--- FinalReport/CD20_3/xeXang.cpp
+++
@@ -1,9 +0,0 @@
-#include "xeXang.h"
-
-xeXang::xeXang(string soKhung, string soMay, double dungTich, string mauSac, string tenHangSX,
-               int soChoNgoi, int namSX, string xuatXu, double giaNhapKhau)
-    : Car(soKhung, soMay, dungTich, mauSac, tenHangSX, soChoNgoi, namSX, xuatXu, giaNhapKhau) {}
-    
-double xeXang::ThueTieuThuDB() {
-    return 0.6 * (giaNhapKhau + 0.7 * giaNhapKhau);
-}
--- FinalReport/CD20_3/xeXangLaiDien.cpp
+++
@@ -1,9 +0,0 @@
-#include "xeLaiXangDien.h"
-
-xeLaiXangDien::xeLaiXangDien(string soKhung, string soMay, double dungTich, string mauSac, string tenHangSX,
-               int soChoNgoi, int namSX, string xuatXu, double giaNhapKhau)
-    : Car(soKhung, soMay, dungTich, mauSac, tenHangSX, soChoNgoi, namSX, xuatXu, giaNhapKhau) {}
-    
-double xeLaiXangDien::ThueTieuThuDB() {
-    return 0.55 * (giaNhapKhau + 0.7 * giaNhapKhau);
-}
